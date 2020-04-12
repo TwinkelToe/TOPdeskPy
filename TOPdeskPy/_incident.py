c5af4cc008064a130c07a1d202c7560aa8a2c326
TwinkelToe <63342939+TwinkelToe@users.noreply.github.com>
2020-04-12 15:00:55 +0200

    Get actions and time spent added

--- TOPdeskPy/_incident.py
+++ TOPdeskPy/_incident.py
@@ -7,12 +7,103 @@ class incident:
         self._topdesk_url = topdesk_url
         self._credpair = credpair
         self.utils = _utils.utils(self._topdesk_url, self._credpair)
+        self.action = self._action(self._topdesk_url, self._credpair)
+        self.request = self._request(self._topdesk_url, self._credpair)
+        self.timespent = self._timespent(self._topdesk_url, self._credpair)
 
-    def get_uuid(self, uuid):
-        return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}".format(uuid)))
+    def get(self, incident):
+        if self.utils.is_valid_uuid(incident):
+            return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}".format(incident)))
+        else:
+            return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}".format(incident)))
+
+    def deescalate(self, incident, reason_id=None):
+        if reason_id:
+            param = {'id': reason_id}
+        if self.utils.is_valid_uuid(incident):    
+            return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/id/{}/deescalate".format(incident), param))
+        else:
+            return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/number/{}/deescalate".format(incident), param))
+
+    def escalate(self, incident, reason_id=None):
+        if reason_id:
+            param = {'id': reason_id}
+        if self.utils.is_valid_uuid(incident):    
+            return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/id/{}/escalate".format(incident), param))
+        else:
+            return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/number/{}/escalate".format(incident), param))
+
+    def get_progress_trail(self, incident, inlineimages=False, non_api_attachments_url=False, page_size=100):
+        ext_uri= { 'inlineimages': inlineimages, 'non_api_attachments_url': non_api_attachments_url }
+        if self.utils.is_valid_uuid(incident):
+            return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}/progresstrail".format(incident), page_size=page_size, extended_uri=ext_uri))
+        else:
+            return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}/progresstrail".format(incident), page_size=page_size, extended_uri=ext_uri))
+
+    class _action:
         
-    def get_number(self, number):
-        return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}".format(number)))
+        def __init__(self, topdesk_url, credpair):
+            self._topdesk_url = topdesk_url
+            self._credpair = credpair
+            self.utils = _utils.utils(self._topdesk_url, self._credpair)
+
+        def get_list(self, incident, inlineimages=False, non_api_attachments_url=False, page_size=100):
+            ext_uri= { 'inlineimages': inlineimages, 'non_api_attachments_url': non_api_attachments_url }
+            if self.utils.is_valid_uuid(incident):
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}/actions".format(incident), page_size=page_size, extended_uri=ext_uri))
+            else:
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}/actions".format(incident), page_size=page_size, extended_uri=ext_uri))
+
+        def get(self, incident, actions_id, inlineimages=False, non_api_attachments_url=False):
+            ext_uri= { 'inlineimages': inlineimages, 'non_api_attachments_url': non_api_attachments_url }
+            if self.utils.is_valid_uuid(incident):
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}/actions/{}".format(incident, actions_id), page_size=10, extended_uri=ext_uri))
+            else:
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}/actions/{}".format(incident, actions_id), page_size=10, extended_uri=ext_uri))
+
+    class _request:
+
+        def __init__(self, topdesk_url, credpair):
+            self._topdesk_url = topdesk_url
+            self._credpair = credpair
+            self.utils = _utils.utils(self._topdesk_url, self._credpair)
+
+        def get_list(self, incident, inlineimages=False, non_api_attachments_url=False, page_size=100):
+            ext_uri= { 'inlineimages': inlineimages, 'non_api_attachments_url': non_api_attachments_url }
+            if self.utils.is_valid_uuid(incident):
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}/requests".format(incident), page_size=page_size, extended_uri=ext_uri))
+            else:
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}/requests".format(incident), page_size=page_size, extended_uri=ext_uri))
+
+        def get(self, incident, request_id, inlineimages=False, non_api_attachments_url=False):
+            ext_uri= { 'inlineimages': inlineimages, 'non_api_attachments_url': non_api_attachments_url }
+            if self.utils.is_valid_uuid(incident):
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}/actions/{}".format(incident, request_id), page_size=10, extended_uri=ext_uri))
+            else:
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}/actions/{}".format(incident, request_id), page_size=10, extended_uri=ext_uri))
+
+    class _timespent:
+
+        def __init__(self, topdesk_url, credpair):
+            self._topdesk_url = topdesk_url
+            self._credpair = credpair
+            self.utils = _utils.utils(self._topdesk_url, self._credpair)
+
+        def get(self, incident):
+            if self.utils.is_valid_uuid(incident):    
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/id/{}/timespent".format(incident)))
+            else:
+                return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/number/{}/timespent".format(incident)))
+
+        def register(self, incident, timespent, **kwargs):
+            param = {}
+            if kwargs:
+                param = kwargs 
+            param['timespent'] =  timespent            
+            if self.utils.is_valid_uuid(incident):    
+                return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/id/{}/timespent".format(incident), param))
+            else:
+                return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/number/{}/timespent".format(incident), param))
 
     def durations(self):
         return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/durations"))
@@ -122,13 +213,13 @@ class incident:
         if self.utils.is_valid_uuid(incident):
             return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/id/{}/unarchive".format(incident), None))
         else:
-            return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/number/{}/unarchive".format(incident), param))
+            return self.utils.handle_topdesk_response(self.utils.put_to_topdesk("/tas/api/incidents/number/{}/unarchive".format(incident), None))
 
     def get_list(self, archived=False, page_size=100, **kwargs):
         # reqeust_uri = "&".join("=".join(_) for _ in kwargs.items())
         # print(reqeust_uri)
         # URL encode.
-        return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/", archived, page_size, extended_uri=kwargs))
+        return self.utils.handle_topdesk_response(self.utils.request_topdesk("/tas/api/incidents/", archived, page_size=page_size, extended_uri=kwargs))
 
 if __name__ == "__main__":
     pass
